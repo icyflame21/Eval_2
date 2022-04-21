098d237817923bc9a0ffcac90fa0f6a56830c41d
biswa-ranjan-subudhi <biswaranjan.cuh@gmail.com>
2022-04-21 09:51:39 +0530

    Added Files

--- src/App.js
+++ src/App.js
@@ -1,23 +1,43 @@
-import logo from './logo.svg';
+import {
+  Routes,
+  Route
+} from 'react-router-dom';
+
 import './App.css';
+import {
+  NavBar
+} from './components/NavBar';
+import {
+  AboutUs
+} from './pages/About';
+import {
+  Contact
+} from './pages/Contact';
+import {
+  Users
+} from './pages/Users';
+import {
+  Login
+} from './pages/Login';
+import {
+  Home
+} from './pages/Home';
+import {
+  UserDetails
+} from './pages/UserDetails';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <NavBar />
+      <Routes>
+        <Route path="/" element={<Home />}/>
+        <Route path="/about-us" element={<AboutUs />}/>
+        <Route path="/contact" element={<Contact />}/>
+        <Route path="/users" element={<Users />}/>
+        <Route path="/login" element={<Login />}/>
+        <Route path="/users/:userId" element={<UserDetails />}/>
+      </Routes>
     </div>
   );
 }
